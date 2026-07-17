7bc9b82f1d5c82e604c53128ca920cd2ff461b11
suyogsawant07 <suyogpatilsawant@gmail.com>
2026-07-17 07:49:52 +0530

    Add files via upload

---
+++ Practical1.java
@@ -0,0 +1,50 @@
+public class Practical1{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+    int age=19;
+    double cgpa= 8.49;
+    char grade='A';
+    String Name="Tarun";
+    boolean isStudent=true;
+    System.out.println("Age: "+age);
+    System.out.println("CGPA: "+cgpa);
+    System.out.println("Name: "+Name);
+    System.out.println("Grade: "+grade);
+    System.out.println("Student: "+isStudent);
+
+
+    int marks=99;
+    double total=marks;
+    System.out.println("Implicit Type Casting");
+    System.out.println("Marks: "+marks);
+    System.out.println("Total Marks:"+total );
+
+    double Cgpa=8.49;
+    int totalcgpa=(int)cgpa;
+    System.out.println("Explicit Type Casting");
+    System.out.println("Double value: "+Cgpa);
+    System.out.println("Converted to Integer: "+totalcgpa);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
